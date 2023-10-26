dddf5047948567097622cdaa40244f9cfb77febc
Opilane TTHK <opilane@tthk.ee>
2023-10-26 12:06:52 +0300

    As a.. #table-id#186340825

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const express = require('express')
+const cors= require('cors')
 const app = require('express')()
 const port= 8080
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
+
+const yamljs = require('yamljs')
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+
+app.use(cors())
+app.use(express.json())
+
 
 app.get('/games', (req, res) => {
     res.send(["Witcher 3", "Cyberpunk 2077"])
@@ -22,12 +30,39 @@ app.get('/games', (req,res)=>{
     res.send(games)
 })
 
+app.post('/games', (req,res)=>{
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).send({error: 'One or all params are missing'})
+    }
+    let game= {
+        id: games.length +1,
+        price: req.body.price,
+        name: req.body.name
+    }
+
+    games.push(game)
+    res.status(201)
+        .location(`${getBaseUrl(req)}/games/${games.length}`)
+        .send(game)
+})
+
 app.get('/games/:id', (req, res)=>{
     if (typeof games[req.params.id -1] === 'undefined'){
         return res.status(404).send({error:"Game not found"})
     }
     res.send(games[req.params.id -1])
 })
+
+
+app.delete('/games/:id', (req,res) => {
+    if (typeof games[req.params.id -1] === 'undefined'){
+        return res.status(404).send({error: "Game not found"})
+    }
+    games.splice(req.params.id - 1,1)
+
+    res.status(204).send({error:"No content"})
+})
+
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
